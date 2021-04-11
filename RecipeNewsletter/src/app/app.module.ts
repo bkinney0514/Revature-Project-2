50105da632936f94a3079cd8022e927665b1f81e
Brian Kinney <bkinney0514@gmail.com>
2021-04-11 16:49:38 -0400

    Added all recipe components, added a navbar to the page, added pictures for recipes, set up Routing

--- RecipeNewsletter/src/app/app.module.ts
+++ RecipeNewsletter/src/app/app.module.ts
@@ -4,13 +4,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChickenParmComponent } from './chicken-parm/chicken-parm.component';
-import { TestComponent } from './test/test.component';
+import { DinnerRollsComponent } from './dinner-rolls/dinner-rolls.component';
+import { VeganPancakesComponent } from './vegan-pancakes/vegan-pancakes.component';
+import { WesternOmeletComponent } from './western-omelet/western-omelet.component';
+import { PaniniComponent } from './panini/panini.component';
+import { GreekPastaSaladComponent } from './greek-pasta-salad/greek-pasta-salad.component';
+import { ChiliComponent } from './chili/chili.component';
+import { VeganBurgerComponent } from './vegan-burger/vegan-burger.component';
+import { BrowniesComponent } from './brownies/brownies.component';
+import { CheesecakeComponent } from './cheesecake/cheesecake.component';
+import { BreakfastComponent } from './breakfast/breakfast.component';
+import { HomeComponent } from './home/home.component';
+import { LunchComponent } from './lunch/lunch.component';
+import { DinnerComponent } from './dinner/dinner.component';
+import { VeganComponent } from './vegan/vegan.component';
+import { DessertComponent } from './dessert/dessert.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ChickenParmComponent,
-    TestComponent
+    DinnerRollsComponent,
+    VeganPancakesComponent,
+    WesternOmeletComponent,
+    PaniniComponent,
+    GreekPastaSaladComponent,
+    ChiliComponent,
+    VeganBurgerComponent,
+    BrowniesComponent,
+    CheesecakeComponent,
+    BreakfastComponent,
+    HomeComponent,
+    LunchComponent,
+    DinnerComponent,
+    VeganComponent,
+    DessertComponent
   ],
   imports: [
     BrowserModule,
--- RecipeNewsletter/src/app/dinner-rolls/dinner-rolls.component.spec.ts
+++ RecipeNewsletter/src/app/dinner-rolls/dinner-rolls.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestComponent } from './test.component';
+import { DinnerRollsComponent } from './dinner-rolls.component';
 
-describe('TestComponent', () => {
-  let component: TestComponent;
-  let fixture: ComponentFixture<TestComponent>;
+describe('DinnerRollsComponent', () => {
+  let component: DinnerRollsComponent;
+  let fixture: ComponentFixture<DinnerRollsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TestComponent ]
+      declarations: [ DinnerRollsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TestComponent);
+    fixture = TestBed.createComponent(DinnerRollsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
